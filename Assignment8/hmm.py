93827d16b912070025e6117645abe28adc2e221d
Jay <jay.lecavalier@colorado.edu>
2015-11-12 00:48:18 -0700

    calculates emission probabilities. Need to test

--- Assignment8/hmm.py
+++ Assignment8/hmm.py
@@ -1,8 +1,35 @@
 class Hmm:
   def __init__(self,data,obs):
-  	self.cond_probs = {}
-  	self.tran_probs = {}
-  	self.init_probs = {}
+  	print("\nInitializing Hidden Markov Model. This may take a bit...\n")
+  	print("\nGenerating emission probabilities...")
+  	self.emis_probs = generate_emis_probs(data,obs)
+  	print("Done!\n\nGenerating transition probabilities...")
+  	self.tran_probs = []
+  	print("Done!\n\nGenerating initial probabilities...")
+  	self.init_probs = []
+  	print("Done!\n\n")
+
+def generate_emis_probs(data,obs):
+  emis_probs = []
+  for i in range(97,123):
+    for j in range(97,123):
+      p = single_emis_prob(chr(i),chr(j),data,obs)
+      emis_probs.append([(chr(i),chr(j)),p])
+  return emis_probs
+
+def single_emis_prob(o,s,data,obs):
+  s_count = 0
+  o_count = 0
+  for i in range(len(data)):
+  	if data[i] == s:
+  	  s_count += 1
+  	  if obs[i] == o:
+  	  	o_count += 1
+  (num,den) = laplace_smoothing(o_count,s_count)
+  return float(num) / float(den)
+
+def laplace_smoothing(num_out,num_pos):
+  return (num_out+1,num_pos+26)
 
 def get_data_and_obs(f):
   data = []
@@ -15,3 +42,5 @@ def get_data_and_obs(f):
 
 if __name__ == "__main__":
   (data,obs) = get_data_and_obs(open("typos20.data"))
+  assert len(data) == len(obs)
+  hmm = Hmm(data,obs)
