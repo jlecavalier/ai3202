1a6200f4700d7df78185a03e14042ff7ee015845
Jay LeCavalier <jay.lecavalier@colorado.edu>
2015-11-12 14:41:00 -0700

    Need to comment, then submit

--- Assignment8/hmm.py
+++ Assignment8/hmm.py
@@ -1,23 +1,36 @@
 class Hmm:
   def __init__(self,data,obs):
-  	print("\nInitializing Hidden Markov Model. This may take a bit...\n")
+  	print("\nInitializing Hidden Markov Model. This may take a bit...")
   	print("\nGenerating emission probabilities...")
   	self.emis_probs = generate_emis_probs(data,obs)
   	print("Done!\n\nGenerating transition probabilities...")
-  	self.tran_probs = []
+  	self.tran_probs = generate_tran_probs(data)
   	print("Done!\n\nGenerating initial probabilities...")
   	self.init_probs = []
   	print("Done!\n")
   	self.verify_probs()
-
+  
   def verify_probs(self):
-	print("Checking whether all probabilities are distributed normally...")
-  	for i in range(26):
-  	  prob = 0.0
-	  for j in range(26):
-	    prob += self.emis_probs[i+(26*j)][1]
-	  assert (abs(1 - prob) <= 0.000000000000001)
-	print("Everything okay!\n")
+    print("Checking whether all probabilities are distributed normally...")
+    for i in range(26):
+      e_prob = 0.0
+      t_prob = 0.0
+      for j in range(26):
+        e_prob += self.emis_probs[i+(j*26)][1]
+        t_prob += self.tran_probs[i+(j*26)][1]
+      assert (abs(1-e_prob) <= 0.000000000000001)
+      assert (abs(1-t_prob) <= 0.000000000000001)
+    print("Everything okay!\n")
+
+  def display_table(self):
+    print("|\tS1\t|\tS2\t|\tP(S2|S1) (Emission Probability)\t|\tP(S2|S1)Transition Probability")
+    for i in range(26):
+      for j in range(26):
+        o = self.emis_probs[(j*26)+i][0][0]
+        s = self.emis_probs[(j*26)+i][0][1]
+        pe = self.emis_probs[(j*26)+i][1]
+        pt = self.tran_probs[(j*26)+i][1]
+        print("|\t%s\t|\t%s\t|\t%.5f\t \t \t \t \t \t \t|\t%.5f" % (s,o,pe,pt))
 
 def generate_emis_probs(data,obs):
   emis_probs = []
@@ -38,6 +51,25 @@ def single_emis_prob(o,s,data,obs):
   (num,den) = laplace_smoothing(o_count,s_count)
   return float(num) / float(den)
 
+def generate_tran_probs(data):
+  tran_probs = []
+  for i in range(97,123):
+    for j in range(97,123):
+      p = single_tran_prob(chr(i),chr(j),data)
+      tran_probs.append([(chr(i),chr(j)),p])
+  return tran_probs
+
+def single_tran_prob(s_now,s_prev,data):
+  prev_count = 0
+  now_count = 0
+  for i in range(len(data)-1):
+    if data[i] == s_prev:
+      prev_count += 1
+      if data[i+1] == s_now:
+        now_count += 1
+  (num,den) = laplace_smoothing(now_count,prev_count)
+  return float(num) / float(den)
+
 def laplace_smoothing(num_out,num_pos):
   return (num_out+1,num_pos+26)
 
@@ -54,3 +86,4 @@ if __name__ == "__main__":
   (data,obs) = get_data_and_obs(open("typos20.data"))
   assert len(data) == len(obs)
   hmm = Hmm(data,obs)
+  hmm.display_table()
