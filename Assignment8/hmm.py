191ac1a6cee846395f34957c61510d9409f2edd7
Jay LeCavalier <jay.lecavalier@colorado.edu>
2015-11-12 14:56:07 -0700

    ready to submit part I

--- Assignment8/hmm.py
+++ Assignment8/hmm.py
@@ -1,5 +1,6 @@
 class Hmm:
   def __init__(self,data,obs):
+    # First, we generate all the probabilities we need for the HMM
   	print("\nInitializing Hidden Markov Model. This may take a bit...")
   	print("\nGenerating emission probabilities...")
   	self.emis_probs = generate_emis_probs(data,obs)
@@ -8,20 +9,32 @@ class Hmm:
   	print("Done!\n\nGenerating initial probabilities...")
   	self.init_probs = []
   	print("Done!\n")
+
+    # Afterwards, we verify that our results actually make up
+    # a probabilistic distribution (i.e. each appropriate
+    # set of probabilities sum to one)
   	self.verify_probs()
   
+  # This function checks whether each of our probabilities are distributed
+  # If we fix S2 and let S1 be an element of {a,b,c,d,...,z}, then
+  # the big sum of P(S2|S1) should sum to exactly one.
   def verify_probs(self):
-    print("Checking whether all probabilities are distributed normally...")
+    print("Checking whether all probabilities are distributed properly...")
     for i in range(26):
       e_prob = 0.0
       t_prob = 0.0
       for j in range(26):
         e_prob += self.emis_probs[i+(j*26)][1]
         t_prob += self.tran_probs[i+(j*26)][1]
+      # Since there is floating point error in computing the probabilities,
+      # we allow the distribution sums to differ from 1 by at most 10^(-15)
       assert (abs(1-e_prob) <= 0.000000000000001)
       assert (abs(1-t_prob) <= 0.000000000000001)
     print("Everything okay!\n")
 
+  # This function displays all of our probabilities in a nice table.
+  # The table doesn't look very nice in a terminal, so you should probably
+  # pipe it to an output file...
   def display_table(self):
     print("|\tS1\t|\tS2\t|\tP(S2|S1) (Emission Probability)\t|\tP(S2|S1)Transition Probability")
     for i in range(26):
@@ -32,6 +45,7 @@ class Hmm:
         pt = self.tran_probs[(j*26)+i][1]
         print("|\t%s\t|\t%s\t|\t%.5f\t \t \t \t \t \t \t|\t%.5f" % (s,o,pe,pt))
 
+# Generates all the emission probabilities
 def generate_emis_probs(data,obs):
   emis_probs = []
   for i in range(97,123):
@@ -40,6 +54,10 @@ def generate_emis_probs(data,obs):
       emis_probs.append([(chr(i),chr(j)),p])
   return emis_probs
 
+# Given an observation and a state, generate
+# the emission probability. i.e., generate
+# the probability of observing o given that we
+# are in state s.
 def single_emis_prob(o,s,data,obs):
   s_count = 0
   o_count = 0
@@ -51,6 +69,7 @@ def single_emis_prob(o,s,data,obs):
   (num,den) = laplace_smoothing(o_count,s_count)
   return float(num) / float(den)
 
+# Generates all the transition probabilities
 def generate_tran_probs(data):
   tran_probs = []
   for i in range(97,123):
@@ -59,6 +78,9 @@ def generate_tran_probs(data):
       tran_probs.append([(chr(i),chr(j)),p])
   return tran_probs
 
+# Given two states, generate the transition probability.
+# i.e., generate the probability of transitioning to 
+# s_now, given that we are in s_prev.
 def single_tran_prob(s_now,s_prev,data):
   prev_count = 0
   now_count = 0
@@ -70,9 +92,17 @@ def single_tran_prob(s_now,s_prev,data):
   (num,den) = laplace_smoothing(now_count,prev_count)
   return float(num) / float(den)
 
+# Smooths a probability using Laplace smoothing.
+# We add 1 to the numerator and we add the number
+# of possible outputs to the denominator. There
+# are 26 lower case letters, so there are 26 possible
+# outputs.
 def laplace_smoothing(num_out,num_pos):
   return (num_out+1,num_pos+26)
 
+# Converts the data file into two arrays. One
+# contains the raw data, and the other contains
+# our observations.
 def get_data_and_obs(f):
   data = []
   obs = []
